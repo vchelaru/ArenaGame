608e5591d8c2e0cf93945c50e44e1e419f1700b0
kainazzzo <ddatti@outlook.com>
2015-03-09 14:27:54 -0600

    Split the tileset off into an external one

---
+++ ArenaGame/ArenaGameContent/Screens/GameScreen/Tilesets/zelda-tiles.tsx
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" tileheight="16" spacing="0" margin="0" tilewidth="16" firstgid="1" name="zelda-tiles">
+  <image source="..\..\..\zelda-tiles.png" width="2048" height="2048" />
+  <terraintypes />
+  <tile id="62">
+    <properties>
+      <property name="Name (string)" value="SpawnerTile" />
+      <property name="EntityToCreate (string)" value="Entities\EnemySpawner" />
+    </properties>
+  </tile>
+</tileset>
