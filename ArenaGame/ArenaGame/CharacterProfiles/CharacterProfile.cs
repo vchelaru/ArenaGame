49a083936a9dca089ab7a1123115198da9b860cd
kainazzzo <ddatti@outlook.com>
2015-03-09 13:32:27 -0600

    Changed the profile save/load to be list based

--- ArenaGame/ArenaGame/CharacterProfiles/CharacterProfile.cs
+++ ArenaGame/ArenaGame/CharacterProfiles/CharacterProfile.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Xml.Serialization;
+﻿using System.Collections;
 
 namespace ArenaGame.CharacterProfiles
 {
@@ -12,41 +11,5 @@ namespace ArenaGame.CharacterProfiles
         public int Melee { get; set; }
         public int Ranged { get; set; }
         public int Health { get; set; }
-
-        /// <summary>
-        /// Saves the character profile to the filename 
-        /// </summary>
-        /// <param name="filename">Relative path to the save file</param>
-        public void SaveToFile(string filename)
-        {
-            var xs = new XmlSerializer(typeof (CharacterProfile));
-
-            using (var fs = new FileStream(filename, FileMode.Create))
-            {
-                xs.Serialize(fs, this);
-            }
-        }
-
-        /// <summary>
-        /// Reads the character profile from the file
-        /// </summary>
-        /// <param name="filename">Relative path to the save file</param>
-        public void ReadFromFile(string filename)
-        {
-            var xs = new XmlSerializer(typeof (CharacterProfile));
-            CharacterProfile profile;
-
-            using (var fs = new FileStream(filename, FileMode.Open))
-            {
-                profile = (CharacterProfile)xs.Deserialize(fs);
-            }
-
-            this.Name = profile.Name;
-            this.Gold = profile.Gold;
-            this.Speed = profile.Speed;
-            this.Melee = profile.Melee;
-            this.Ranged = profile.Ranged;
-            this.Health = profile.Health;
-        }
     }
 }
---
+++ ArenaGame/ArenaGame/CharacterProfiles/CharacterProfileSave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ArenaGame.CharacterProfiles
+{
+    public static class CharacterProfileSave
+    {
+        public static List<CharacterProfile> Profiles { get; set; }
+
+        /// <summary>
+        /// Saves the character profile to the filename 
+        /// </summary>
+        /// <param name="filename">Relative path to the save file</param>
+        public static void ToFile(string filename = "profiles.xml")
+        {
+            var xs = new XmlSerializer(typeof(List<CharacterProfile>));
+
+            using (var fs = new FileStream(filename, FileMode.Create))
+            {
+                xs.Serialize(fs, Profiles);
+            }
+        }
+
+        /// <summary>
+        /// Reads the character profile from the file
+        /// </summary>
+        /// <param name="filename">Relative path to the save file</param>
+        public static void FromFile(string filename = "profiles.xml")
+        {
+            var xs = new XmlSerializer(typeof(CharacterProfile));
+            List<CharacterProfile> profile;
+
+            using (var fs = new FileStream(filename, FileMode.Open))
+            {
+                profile = (List<CharacterProfile>)xs.Deserialize(fs);
+            }
+
+            Profiles = profile;
+        }
+    }
+}
